b913e8b5cf1a0c86019bb3ea025c482eb1458ae0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-03-31 17:27:28 +0900

    Creating a controller
    お昼専用ページ表示のためのコントローラーを作成

---
+++ app/controllers/lunches_controller.rb
@@ -0,0 +1,5 @@
+class LunchesController < ApplicationController
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :owners
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'menus#index'
-  # get 'notices' => 'notices#index'
+  get 'lunches' => 'lunches#index'
   resources :notices, only: [:index, :new, :create, :show, :destroy]
 end
